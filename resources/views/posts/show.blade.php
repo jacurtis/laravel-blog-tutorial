43309b0fdd67b94a459cecd6a92fad2b1d68b82d
J. Alexander Curtis <jalexandercurtis@gmail.com>
2016-03-05 10:08:19 -0700

    Part 15 - Working with Dates in the Database

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,8 +4,36 @@
 
 @section('content')
 
-	<h1>{{ $post->title }}</h1>
+	<div class="row">
+		<div class="col-md-8">
+			<h1>{{ $post->title }}</h1>
+			
+			<p class="lead">{{ $post->body }}</p>
+		</div>
 
-	<p class="lead">{{ $post->body }}</p>
+		<div class="col-md-4">
+			<div class="well">
+				<dl class="dl-horizontal">
+					<dt>Create At:</dt>
+					<dd>{{ date('M j, Y h:ia', strtotime($post->created_at)) }}</dd>
+				</dl>
+
+				<dl class="dl-horizontal">
+					<dt>Last Updated:</dt>
+					<dd>{{ date('M j, Y h:ia', strtotime($post->updated_at)) }}</dd>
+				</dl>
+				<hr>
+				<div class="row">
+					<div class="col-sm-6">
+						{!! Html::linkRoute('posts.edit', 'Edit', array($post->id), array('class' => 'btn btn-primary btn-block')) !!}
+					</div>
+					<div class="col-sm-6">
+						{!! Html::linkRoute('posts.destroy', 'Delete', array($post->id), array('class' => 'btn btn-danger btn-block')) !!}
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
 
 @endsection
