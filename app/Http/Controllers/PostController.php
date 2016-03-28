ff406cf1ba0bffb5f5d8c1fa2136384393c06c82
J. Alexander Curtis <jalexandercurtis@gmail.com>
2016-03-27 20:18:18 -0600

    Part 24 had a bug, here we fixed it quickly with a nice if statement

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -97,11 +97,21 @@ class PostController extends Controller
     public function update(Request $request, $id)
     {
         // Validate the data
+        $post = Post::find($id);
+        
+        if ($request->input('slug') == $post->slug) {
+            $this->validate($request, array(
+                'title' => 'required|max:255',
+                'body'  => 'required'
+            ));
+        } else {
         $this->validate($request, array(
                 'title' => 'required|max:255',
                 'slug'  => 'required|alpha_dash|min:5|max:255|unique:posts,slug',
                 'body'  => 'required'
             ));
+        }
+
         // Save the data to the database
         $post = Post::find($id);
 
